58f37968375a5ce4ea20fbeecec1e5642120da69
Allan Soares <apsouza@marketdata.com.br>
2020-03-23 18:56:52 -0300

    animacao nome ok

---
+++ HTML/js/navegacao.js
@@ -0,0 +1,10 @@
+
+function animaNome() {
+    $('.anima-nome').addClass('gira1');
+    setTimeout(function () {
+        $('.anima-nome').addClass('gira2');
+    }, 700);
+    setTimeout(function () {
+        $('.anima-nome').addClass('some');
+    }, 1400);
+}
