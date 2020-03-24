36bb584d16c30236789cfe0ce6d1b5fe10dae7e5
Allan Soares <apsouza@marketdata.com.br>
2020-03-24 11:20:06 -0300

    anima itens mosaico

--- HTML/js/navegacao.js
+++ HTML/js/navegacao.js
@@ -7,4 +7,7 @@ function animaNome() {
     setTimeout(function () {
         $('.anima-nome').addClass('some');
     }, 1400);
+    setTimeout(function () {
+        $('.mosaico >div >i').addClass('anima');
+    }, 2100);
 }
